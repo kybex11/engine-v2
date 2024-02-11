26954573b5297208ab84f00246771ea3c6f6e410
kybex11 <artm.22@@internet.ru>
2024-02-11 11:00:34 +0300

    Handle And Control

---
+++ engine/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 style="color: white; font-family: Arial, sans-serif;">Go to?</h1>
+  <br>
+  <button @click="navigateTo('/tilemap')">Go to Tilemap example</button>
+  <br>
+  <button @click="navigateTo('/sample')">Go to Sample</button>
+</template>
+
+<script setup></script>
